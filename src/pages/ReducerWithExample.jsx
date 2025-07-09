173a72c0b80d3c932f6e25011c74e1f21e6c2dbe
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-10 03:22:49 +0600

    Feature:  update task's text

--- src/pages/ReducerWithExample.jsx
+++ src/pages/ReducerWithExample.jsx
@@ -3,6 +3,13 @@ import { useReducer, useState } from 'react'
 const ReducerWithExample = () => {
     const [tasks, dispatch] = useReducer(taskReducer, initialTasks)
 
+    const handleUpdateTask = (task) => {
+        dispatch({
+            type: 'updated',
+            task: task
+        })
+    }
+
     return (
         <>
             <h2 className="card-title text-2xl mb-4">
@@ -16,24 +23,33 @@ const ReducerWithExample = () => {
                 <button className="btn btn-neutral btn-wide ml-4">Add</button>
             </div>
             <div className="flex flex-col mt-4">
-                {tasks.map(task => <Task key={task.id} task={task} />)}
+                {tasks.map(task => (
+                    <Task
+                        key={task.id}
+                        task={task}
+                        handleUpdateTask={handleUpdateTask} />
+                ))}
             </div>
         </>
     )
 }
 
-const Task = ({ task }) => {
+const Task = ({ task, handleUpdateTask }) => {
     const [editing, setEditing] = useState(false)
 
     return (
         <div className="flex flex-row my-1">
             <div className="flex flex-row bg-gray-100 rounded-md px-5 py-3 min-w-80">
-                <input type="checkbox" className="checkbox mr-3" />
+                <input type="checkbox" checked={task.done} className="checkbox mr-3" />
                 {editing ? (
                     <input
                         type="text"
+                        className="input input-bordered w-full max-w-xs input-xs"
                         value={task.text}
-                        className="input input-bordered w-full max-w-xs input-xs" />
+                        onChange={e => handleUpdateTask({
+                            ...task,
+                            text: e.target.value
+                        })} />
                 ) : (
                     <p className="text-md">{task.text}</p>
                 )}
@@ -48,7 +64,23 @@ const Task = ({ task }) => {
     )
 }
 
-const taskReducer = (tasks, action) => { }
+const taskReducer = (tasks, action) => { 
+    switch(action.type) {
+        case 'updated': {
+            return tasks.map(e => {
+                if (e.id === action.task.id) {
+                    return action.task
+                } else {
+                    return e
+                }
+            })
+        }
+
+        default: {
+            throw new Error('Unknown action: ' + action.type)
+        }
+    }
+}
 
 const initialTasks = [
     { id: 0, text: 'Visit Kafka Museum', done: true },
