b2409fc77c45a52e7ace1f6587aa4cf031ae09d7
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-09 22:05:45 +0600

    Add daisyUI and implement basic card structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router'
 
 const HomePage = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-grey-100">
+        <div className="min-h-screen flex items-center justify-center bg-base-200">
             <div className="flex flex-col">
                 <Link to={'/reducer-example'}
-                    className="px-6 py-2 bg-green-700 text-white rounded-lg shadow hover:bg-green-800 transition">
+                    className="btn btn-primary">
                     Reducer with Example
                 </Link>
             </div>
--- src/pages/ReducerWithExample.jsx
+++ src/pages/ReducerWithExample.jsx
@@ -1,5 +1,25 @@
+import { Link } from 'react-router'
+import { ArrowLeftIcon } from 'lucide-react'
+
 const ReducerWithExample = () => {
-    return <h1>This is page body</h1>
+    return (
+        <div className="min-h-screen bg-base-200">
+            <div className="container mx-auto px-4 py-8">
+                <div className="mx-auto max-w-2xl">
+                    <Link to="/" className="btn btn-ghost mb-6">
+                        <ArrowLeftIcon className="size-5" />
+                        Back to Homepage
+                    </Link>
+
+                    <div className="card bg-base-100">
+                        <div className="card-body">
+                            <h2 className="card-title text-2xl mb-4">State management with Reducer</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ReducerWithExample
