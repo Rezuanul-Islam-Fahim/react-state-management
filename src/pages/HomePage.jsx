5e0993720e1b2c352770a8881a7e5f320828ed48
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-09 20:52:30 +0600

    Add tailwind-css library and make basic homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-grey-100">
+            <div className="flex flex-col">
+                <button
+                    className="px-6 py-2 bg-green-700 text-white rounded-lg shadow hover:bg-green-800 transition">
+                    Reducer with Example
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
